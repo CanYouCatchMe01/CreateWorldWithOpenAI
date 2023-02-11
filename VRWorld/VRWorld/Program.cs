9a3973c19d6c49231934ae923b22a4579ffe62ee
Marcus Cazzola <marcus.cazzola@gmail.com>
2023-02-11 14:30:36 +0100

    Started on implementing OpenAI in VR app

--- VRWorld/VRWorld/Program.cs
+++ VRWorld/VRWorld/Program.cs
@@ -3,6 +3,8 @@ using System;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenAI_API.Completions;
 
 namespace VRWorld
 {
@@ -19,6 +21,15 @@ namespace VRWorld
             if (!SK.Initialize(settings))
                 Environment.Exit(1);
 
+            //Open AI
+            var api = new OpenAI_API.OpenAIAPI(); //loads the API key from the .openai file that is in the same directory as the .exe
+            string aiText = "Create a json block from prompt.\nExample:\ntext: Create a blue cube at position one one one\njson: {\"id:\" 0, \"position\" : {\"x\" : 1, \"y\" : 1, \"z\" : 1}, \"shape\" : \"cube\", \"color\" : {\"r\" : 0.0, \"g\" : 0.0, \"b\" : 1.0}}\nReal start with id 0:\ntext: ";
+            string startSequence = "\njson:";
+            string restartSequence = "\ntext:\n";
+            string textInput = "";
+            Task<CompletionResult> generateTask = null;
+
+            //GameObjects are stored in a list
             int myIdCounter = 0;
             List<VRWorld.Object> objects = new List<VRWorld.Object>();
 
@@ -36,8 +47,7 @@ namespace VRWorld
             Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
             floorMaterial.Transparency = Transparency.Blend;
 
-            Pose windowPose = new Pose(0, 0, -0.5f, Quat.LookDir(1, 0, 1));
-            string textInput = "";
+            Pose windowPose = new Pose(0, 0, -0.5f, Quat.LookDir(0, 0, 1));
 
             //var task = Task.Run(async () =>
             //{
@@ -52,14 +62,25 @@ namespace VRWorld
                     Default.MeshCube.Draw(floorMaterial, floorTransform);
 
                 UI.WindowBegin("Open AI chat", ref windowPose, new Vec2(20, 0) * U.cm);
-                UI.Text(textInput);
+                UI.Text(aiText);
                 UI.Input("Input", ref textInput);
                 if (UI.Button("Submit text"))
                 {
-                    //Do some Open AI call
+                    aiText += textInput + startSequence;
+                    generateTask = GenerateAIResponce(api, aiText);
+                   
                     textInput = ""; //Clear input
+                    
                 }
                 UI.WindowEnd();
+
+                if (generateTask != null && generateTask.IsCompleted)
+                {
+                    string responce = generateTask.Result.ToString();
+                    HandleAIResponce(responce, objects, myIdCounter);
+                    aiText += responce + restartSequence;
+                    generateTask = null;
+                }
 
                 foreach(VRWorld.Object o in objects)
                 {
@@ -67,6 +88,27 @@ namespace VRWorld
                 }
             }));
             SK.Shutdown();
+        }
+
+        static async Task<CompletionResult> GenerateAIResponce(OpenAI_API.OpenAIAPI anApi, string aPrompt)
+        {
+            var request = new CompletionRequest(
+                    prompt: aPrompt,
+                    model: OpenAI_API.Models.Model.CushmanCode,
+                    temperature: 0.1,
+                    max_tokens: 100,
+                    top_p: 1.0,
+                    frequencyPenalty: 0.0,
+                    presencePenalty: 0.0,
+                    stopSequences: new string[] { "text:", "json:", "\n" }
+                    );
+            var result = await anApi.Completions.CreateCompletionAsync(request);
+            return result;
+        }
+
+        static void HandleAIResponce(string aResponce, List<VRWorld.Object> someObjects, int someIdCounter)
+        {
+            
         }
     }
 }
