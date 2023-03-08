6eb0a4a6caa7ea9321e3981db0f4ec0cc1ba20b8
Marcus Cazzola <marcus.cazzola@gmail.com>
2023-03-08 19:43:51 +0100

    Now displaying the 3 axis correct

--- VRWorld/VRWorld/ScalingCoordinateSystem.cs
+++ VRWorld/VRWorld/ScalingCoordinateSystem.cs
@@ -10,7 +10,7 @@ namespace VRWorld
     internal class ScalingCoordinateSystem
     {
         Vec3 myCenterScale = Vec3.One * 3.0f * U.cm; //should be 3 cm
-        Vec3 myPinArmScale = new Vec3(10, 1, 1) * U.cm;
+        Vec3 myPinArmScale = new Vec3(10.0f, 0.5f, 0.5f) * U.cm;
         Vec3 myPinHeadScale = Vec3.One * 4.0f * U.cm;
         Model myAlwaysModel;
         Model myLessModel;
@@ -18,7 +18,7 @@ namespace VRWorld
         //Create constuctior
         public ScalingCoordinateSystem()
         {
-            Shader shader = Shader.UI;
+            Shader shader = Shader.Unlit;
             {
                 Material mat = new Material(shader);
                 mat.DepthTest = DepthTest.Always;
@@ -38,13 +38,17 @@ namespace VRWorld
         {
             Quat quatRotation = Quat.FromAngles(aRotation);
             //Matrix matrix = anObjectPose.ToMatrix() * Matrix.R(quatRotation) * Matrix.T(Vec3.Right * myPinArmScale.x) *  Matrix.S(myPinArmScale);
-            Matrix matrix = Matrix.S(myPinArmScale) * Matrix.R(quatRotation) * Matrix.T(Vec3.Forward * myPinArmScale.x / 2.0f) * anObjectPose.ToMatrix(); //* Matrix.S(myPinArmScale);
+            Matrix matrix = Matrix.S(myPinArmScale) * Matrix.T(Vec3.Right * myPinArmScale.x / 2.0f) * Matrix.R(quatRotation) * anObjectPose.ToMatrix();
+            //Matrix matrix = Matrix.R(quatRotation) * Matrix.S(myPinArmScale) ** anObjectPose.ToMatrix();
+            //Matrix matrix = Matrix.R(quatRotation) * Matrix.T(Vec3.Forward * myPinArmScale.x / 2.0f) * Matrix.S(myPinArmScale) * anObjectPose.ToMatrix(); //* Matrix.S(myPinArmScale);
 
             aModel.Draw(matrix, color);
         }
         private void Draw(Model aModel, Pose anObjectPose)
         {
             //aModel.Draw(anObjectPose.ToMatrix(myCenterScale), new Color(0.5f, 0.5f, 0.5f));
+            DrawPin(aModel, anObjectPose, new Vec3(0, 0, 90), new Color(1.0f, 0.0f, 0.0f));
+            DrawPin(aModel, anObjectPose, new Vec3(0, 0, 0), new Color(0.0f, 1.0f, 0.0f));
             DrawPin(aModel, anObjectPose, new Vec3(0, 90, 0), new Color(0.0f, 0.0f, 1.0f));
         }
         public void Draw(Pose anObjectPose)
